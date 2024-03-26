a8b9d6f02932ad6697f86e4c4ec0fd07dfa6196b
Raja Singh <rajasgh18@gmail.com>
2024-03-26 20:16:29 +0530

    importing to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Highlights from './components/highlights';
+import Model from './components/model';
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-2xl">App</h1>
-    </div>
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
   );
 };
 
